67e0add426fbf4e8f9da108c51aaac26243cbf4f
Silvio Bassi <silviobassi1@hotmail.com>
2023-02-25 17:29:07 -0300

    refactoring

--- backend/src/main/java/com/dcconnect/minimizingwaste/core/security/WebMvcConfig.java
+++ backend/src/main/java/com/dcconnect/minimizingwaste/core/security/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.dcconnect.minimizingwaste.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
